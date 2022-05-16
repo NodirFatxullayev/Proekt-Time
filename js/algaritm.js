27d276eec04783c4219e885951fe12a40bae3bfa
NodirFatxullayev <nfatxullayev@internet.ru>
2022-05-16 13:21:15 +0500

    proekt vertion4 default holatda ham soat va minut chiqadigan qilindi

--- js/algaritm.js
+++ js/algaritm.js
@@ -1,16 +1,6 @@
 
-// let x = 0;
-// setInterval(displayHello, 2000);
-
-// function displayHello() {
-//     ++x;
-//     const para = document.createElement("div");
-//     para.innerText = "This is a paragraph" + `${x}`;
-//     para.classList.add("box");
-//     document.body.appendChild(para);
-//     setInterval(function(){ para.remove()}, 2800);
-// }
-
+document.getElementById("minut").innerText = new Date().getMinutes();
+document.getElementById("hour").innerText = new Date().getHours();
 setInterval(secund, 1000);
 
 function secund() {
