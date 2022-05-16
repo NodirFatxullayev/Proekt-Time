2526bb9d71023e42332207d92e8c947451f438a7
NodirFatxullayev <nfatxullayev@internet.ru>
2022-05-16 11:37:37 +0500

    proekt vertion3 minut , secund, hour finesh

--- js/algaritm.js
+++ js/algaritm.js
@@ -16,7 +16,18 @@ setInterval(secund, 1000);
 function secund() {
     const secund = document.createElement("div");
     secund.innerText = new Date().getSeconds();
-    secund.classList.add("sec");
     document.getElementById("secund").appendChild(secund);
     setInterval(function(){secund.remove()}, 1500);
+
+    
+    if(new Date().getSeconds() < 1) {
+        const minut = document.createElement("div");
+        minut.innerText = new Date().getMinutes();
+        document.getElementById("minut").appendChild(minut);
+    }
+    if(new Date().getMinutes() < 1) {
+        const hour = document.createElement("div");
+        hour.innerText = new Date().getHours();
+        document.getElementById("hour").appendChild(hour);
+    }
 }
