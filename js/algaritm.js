f104e09782a8683dc12031235ed26dad85e2824f
NodirFatxullayev <nfatxullayev@internet.ru>
2022-05-16 10:24:26 +0500

    proekt vertion1 default files

---
+++ js/algaritm.js
@@ -0,0 +1,12 @@
+
+// let x = 0;
+// setInterval(displayHello, 2000);
+
+// function displayHello() {
+//     ++x;
+//     const para = document.createElement("div");
+//     para.innerText = "This is a paragraph" + `${x}`;
+//     para.classList.add("box");
+//     document.body.appendChild(para);
+//     setInterval(function(){ para.remove()}, 2800);
+// }
